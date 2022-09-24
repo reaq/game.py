8d72406b88af85440f8befec6a24da3d7930be53
reaq <113785844+reaq@users.noreply.github.com>
2022-09-25 00:42:43 +0800

    Add files via upload

---
+++ game.py
@@ -0,0 +1,14 @@
+answer=input('The card is:')
+answer=input('Higher or Lower [h/l]:')
+answer=input('Next card was:')
+answer=input('Your score is:')
+answer=input('Play again? [y/n]')
+
+answer=input('The card is:')
+answer=input('Higher or Lower [h/l]:')
+answer=input('Next card was:')
+answer=input('Your score is:')
+answer=input('Play again? [y/n]')
+
+
+
